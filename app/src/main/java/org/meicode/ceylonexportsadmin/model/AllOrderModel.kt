226268ccdcf50ee79cd5c2535ee9b4542b6252b6
RanaweeraIU <it21315732@my.sliit.lk>
2023-05-06 20:11:29 +0530

    Proceed Order

---
+++ app/src/main/java/org/meicode/ceylonexportsadmin/model/AllOrderModel.kt
@@ -0,0 +1,10 @@
+package org.meicode.ceylonexportsadmin.model
+
+data class AllOrderModel(
+    val name : String? = "",
+    val orderID : String? = "",
+    val userID : String? = "",
+    val status : String? = "",
+    val productID : String? = "",
+    val price : String? = "",
+)
